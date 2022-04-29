430d9e2bde3335119e13d57fd8e147fd4a9a798f
GabrielaRaluca <gabriela.br141995@gmail.com>
2022-04-29 21:29:14 +0300

    added missing endpoints

--- src/main/java/com/ing/test/controllers/ProductController.java
+++ src/main/java/com/ing/test/controllers/ProductController.java
@@ -1,31 +1,25 @@
 package com.ing.test.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.ing.test.exception.BadRequestException;
 import com.ing.test.model.Product;
-import com.ing.test.repository.ProductRepository;
+import com.ing.test.services.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
-    private ObjectMapper objectMapper;
-    private final ProductRepository productRepository;
+    private final ProductService productService;
 
-    public ProductController(final ProductRepository productRepository) {
-        this.objectMapper = new ObjectMapper();
-        this.productRepository = productRepository;
+    public ProductController(final ProductService productService) {
+        this.productService = productService;
     }
 
     @GetMapping("/{id}")
     ResponseEntity getProduct(@PathVariable Long id){
-        Optional<Product> productOptional = productRepository.findById(id);
+        Optional<Product> productOptional = productService.getProduct(id);
 
         if(!productOptional.isPresent()) {
             throw new BadRequestException("No travel post exists with id " + id);
@@ -37,9 +31,38 @@ public class ProductController {
 
     @GetMapping()
     ResponseEntity getAllProducts() {
-        Iterable<Product> products = productRepository.findAll();
-        return ResponseEntity.ok(products);
+        return ResponseEntity.ok(productService.getProducts());
     }
 
+    @PostMapping()
+    ResponseEntity createProduct(@RequestBody Product product) {
+        Optional<Product> productOptional = productService.createProduct(product);
+        if (!productOptional.isPresent()) {
+            throw new BadRequestException("Product with the same name already exists!");
+        } else {
+            return ResponseEntity.ok(productOptional.get());
+        }
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        Optional<Product> productOptional = productService.updateProduct(id, product);
+        if (!productOptional.isPresent()) {
+            throw new BadRequestException("Product with id " + id + " does not exist!");
+        } else {
+            return ResponseEntity.ok(productOptional.get());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity deleteProduct(@PathVariable Long id) {
+
+        Optional<Product> productOptional = productService.deleteProduct(id);
+        if (!productOptional.isPresent()) {
+            throw new BadRequestException("Product with id " + id + " does not exist!");
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
 
 }
---
+++ src/main/java/com/ing/test/services/ProductService.java
@@ -0,0 +1,55 @@
+package com.ing.test.services;
+
+import com.ing.test.model.Product;
+import com.ing.test.repository.ProductRepository;
+import org.springframework.security.core.parameters.P;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(final ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Optional<Product> getProduct(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Iterable<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> createProduct(Product newProduct) {
+        Optional<Product> productOptional = productRepository.findById(newProduct.getId());
+
+        if (productOptional.isPresent()) {
+            return Optional.empty();
+        } else {
+            return Optional.of(productRepository.save(newProduct));
+        }
+    }
+
+    public Optional<Product> deleteProduct(Long id) {
+        Optional<Product> productOptional = productRepository.findById(id);
+        if (productOptional.isPresent()) {
+            productRepository.deleteById(id);
+            return Optional.of(productOptional.get());
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Product> updateProduct(Long id, Product product) {
+        Optional<Product> productOptional = productRepository.findById(id);
+        if (productOptional.isPresent()) {
+            product.setId(productOptional.get().getId());
+            return Optional.of(productRepository.save(product));
+        } else {
+            return Optional.empty();
+        }
+    }
+}
